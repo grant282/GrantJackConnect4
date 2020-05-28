aaa904f065da63677a5d9876937733c919c16ab2
grant282 <66043745+grant282@users.noreply.github.com>
2020-05-27 21:15:21 -0400

    Add files via upload

---
+++ Board.java
@@ -0,0 +1,70 @@
+package ics4ustart;
+
+import java.util.Random;
+
+/**
+ * Represents a 2 dimensional Board for Grid based games.
+ * @author Hutchison
+ * @version 1.0
+ *
+ */
+public class Board {
+	
+	private Cell[][] board;
+	private int rows;
+	private int cols;
+
+	/**
+	 * Constructor for Boards.
+	 * @param aRows number of rows in board
+	 * @param aCols number of columns in board
+	 */
+	public Board(int aRows, int aCols) {
+		board = new Cell[aRows][aCols];
+		rows = aRows;
+		cols = aCols;
+		for (int i = 0; i < rows; i++) {
+			for (int j = 0; j < cols; j++) {
+				board[i][j] = new Cell(CellState.EMPTY); // no color
+			}
+		}
+	}
+	
+	/**
+	 * Obtain the current number of rows.
+	 * @return number of rows
+	 */
+	public int getRows(){
+		return rows;
+	}
+	
+	/**
+	 * Obtain the current number of columns.
+	 * @return number of columns
+	 */
+	public int getCols(){
+		return cols;
+	}
+	
+	/**
+	 * Check if a proposed location is valid.
+	 * @param rowIndex row index to check
+	 * @param colIndex column index to check
+	 * @return true if index value is valid, otherwise false
+	 */
+	public boolean isValid(int rowIndex, int colIndex){
+		return (rowIndex>=0 && rowIndex < rows) && (colIndex>=0 && colIndex < cols);
+	}
+	
+	
+	
+	public void display() {
+		System.out.println("BOARD");
+		for (int i = 0; i < rows; i++) {
+			for (int j = 0; j < cols; j++) {
+				System.out.printf("%s ", board[i][j]);
+			}
+			System.out.println();
+		}
+	}
+}
---
+++ Driver.java
@@ -0,0 +1,61 @@
+package ics4ustart;
+
+import java.util.Scanner;
+import java.util.ArrayList;
+import java.util.List;
+
+
+public class Driver {
+
+	public static void main(String[] args) {
+		
+		
+
+		// Setup constants for the Board
+		final int ROWS = 6;
+		final int COLS = 7;
+
+		// create the board
+		Board board = new Board(ROWS, COLS);
+		board.display();
+		
+		// console input
+		Scanner in = new Scanner(System.in);
+		boolean done = false;
+		String value = "";
+		int column = 0;
+
+		while (!done) {
+			column = getColumn(in, 1, COLS); // include min and max
+			
+			board.display();
+		}
+	}
+	
+/**
+ * Helper method to ensure column value is valid.
+ * @param in
+ * @return
+ */
+	private static int getColumn(Scanner in, int min, int max) {
+		boolean valid = false;
+		int column = 0;
+		
+		while (!valid) {
+			String prompt = String.format("Which column (%d,%d) :",min,max); 
+			System.out.print(prompt);
+			if (in.hasNextInt()) {
+				column = in.nextInt();
+				if (column >= min && column <= max) {
+					valid = true;
+				} else {
+					System.out.println("Invalid numeric value provided.");
+				}
+			} else {
+				System.out.println("Not a number.");
+			}
+			in.nextLine();
+		}
+		return column;
+	}
+}
